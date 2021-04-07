bc8fd7d7fd7dbfa2d1f701f7bd1b324fe3948440
sean2599 <37426319+sean2599@users.noreply.github.com>
2021-04-07 16:54:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,35 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/vernesong/OpenClash.git
+
+# OpenClash
+# 解压下载好的 SDK
+tar xjf OpenWrt-SDK-ar71xx-for-linux-x86_64-gcc-4.8-linaro_uClibc-0.9.33.2.tar.bz2
+cd OpenWrt-SDK-ar71xx-*
+
+# Clone 项目
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+
+# 开始编译
+
+# 先回退到SDK主目录
+cd ../..
+make package/luci-app-openclash/luci-app-openclash/compile V=99
+
+# IPK文件位置
+./bin/ar71xx/packages/base/luci-app-openclash_0.39.7-beta_all.ipk
+# 同步源码
+cd package/luci-app-openclash/luci-app-openclash
+git pull
